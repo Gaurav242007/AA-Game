76cdbd4951ce5e9c6ec28d6de9a8f5fbd80397a8
Gaurav242007 <gaurav2499kumar+1@gmail.com>
2023-05-28 15:15:04 +0530

    Added Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadLevels()
+    {
+        FindObjectOfType<AudioController>().GetComponent<AudioController>().PlaySelectUi();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitApplication()
+    {
+        FindObjectOfType<AudioController>().GetComponent<AudioController>().PlaySelectUi();
+        Application.Quit();
+    }
+}
